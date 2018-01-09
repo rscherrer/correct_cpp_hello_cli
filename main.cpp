8bf103dd71980e03a3acc7f8591f87f4084b6fd3
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-09 14:50:43 +0100

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-main(argc, argv) 
-{
+#include <iostream>
 
+int main(int argc, char* argv[])
+{
+  std::cout << "Hello";
+  if (argc > 1)
+  {
+    std::cout << ' ' << argv[1];
+  }
+  std::cout << '\n';
 }
