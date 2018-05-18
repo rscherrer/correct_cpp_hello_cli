9fb0e3585b5eba8245e255f4f7ddb7a066ba0e1d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-18 14:37:30 +0200

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-
-/// hello_cli main function
-int main(int argc, char* argv[])
+main(argc, argv) 
 {
-  std::cout << "Hello";
-  if (argc > 1)
-  {
-    std::cout << ' ' << argv[1];
-  }
-  std::cout << '\n';
+
 }
