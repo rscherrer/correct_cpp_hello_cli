a3317679903cc7bd1196ee03cd2fff90e598f5ea
rscherrer <raphael.scherrer@evobio.eu>
2019-06-12 15:51:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
-main(argc, argv) 
-{
+#include <iostream>
+
+/// Main function
+int main(int argc, char * argv[]) {
+
+    // Write Hello to the screen followed by the argument
+    std::cout << "Hello ";
+
+    if(argc > 1) {
+
+        std::cout << argv[1];
+
+    }
+
+    std::cout << '\n';
+
+    return 0;
 
 }
